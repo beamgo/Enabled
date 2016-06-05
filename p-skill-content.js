9e4b79e1f6ad900fdc89c983f98eca78c44cbeab
beamgo <beam-go@hotmail.com>
2016-06-06 00:19:17 +0700

    content

--- p-skill-content.js
+++ p-skill-content.js
@@ -5,13 +5,13 @@
     ,"body":"ทักษะการพิมพ์ดีดซึ่งในอดีตเป็นทักษะเฉพาะอาชีพเลขานุการหรือพนักงานพิมพ์ดีดเท่านั้น แต่ปัจจุบันได้กลายเป็นทักษะสำหรับทุกคนแล้ว เพราะการมีทักษะดังกล่าวติดตัวจะช่วยให้การใช้งานคอมพิวเตอร์สะดวกมากขึ้น สามารถป้อนข้อมูลข่าวสารได้มากและแม่นยำด้วยเวลาที่น้อยลง และที่สำคัญคือเป็นทักษะพื้นฐานที่ต้องการของนายจ้างเป็นอันดับต้นๆเลยทีเดียว"
     ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ"]
     ,"course":[
-              {"name":"ฝึกพิมพ์ไทย-อังกฤษ","link":"http://www.thaiedu.net/typing/","pic":"imgcontent/"}
-              ,{"name":"ฝึกพิมพ์กับโยงใยไทยศึกษา","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":""}
-              ,{"name":"บทเรียนการพิมพ์สัมผัส","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":"imgcontent/"}
+              {"name":"ฝึกพิมพ์ไทย-อังกฤษ","link":"http://www.thaiedu.net/typing/","pic":""}
+              ,{"name":"ฝึกพิมพ์กับโยงใยไทยศึกษา","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":"imgcontent/it_low_1_online_2.png"}
+              ,{"name":"บทเรียนการพิมพ์สัมผัส","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":""}
               ]
     ,"center":[
-              {"name":"มูลนิธิส่งเสริมและพัฒนาคนพิการ รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","link":"http://www.tdf.or.th/14857695/มูลนิธิส่งเสริมและพัฒนาคนพิการ-รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","pic":"imgcontent/"}
-              ,{"name":"ฝึกทักษะคนพิการและบ้านวิถีชีวิตอิสระ","link":"http://snmrc57.snmrc.go.th/index.php?option=com_content&view=article&id=15&Itemid=16","pic":"imgcontent/"}
+              {"name":"มูลนิธิส่งเสริมและพัฒนาคนพิการ รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","link":"http://www.tdf.or.th/14857695/มูลนิธิส่งเสริมและพัฒนาคนพิการ-รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","pic":"imgcontent/it_low_1_center_1.png"}
+              ,{"name":"ฝึกทักษะคนพิการและบ้านวิถีชีวิตอิสระ","link":"http://snmrc57.snmrc.go.th/index.php?option=com_content&view=article&id=15&Itemid=16","pic":"imgcontent/it_low_1_center_2.png"}
               ]
     }
 
@@ -26,83 +26,111 @@
               ]
     ,"center":[
               {"name":"โครงการฝึกอบรมการใช้คอมพิวเตอร์และเทคโนโลยีสารสนเทศเพื่อส่งเสริมการพัฒนาคุณภาพชีวิตคนตาบอด","link":"https://uso.nbtc.go.th/TH/project-detail.php?proID=31","pic":"imgcontent/it_low_2_center_1.png"}
-              ,{"name":"ฝึกทักษะคนพิการและบ้านวิถีชีวิตอิสระ","link":"http://snmrc57.snmrc.go.th/index.php?option=com_content&view=article&id=15&Itemid=16","pic":"imgcontent/"}
               ]
     }
 
     ,{
-      "topic":"สอนการพิมดีด"
-    ,"body":"ทักษะการพิมพ์ดีดซึ่งในอดีตเป็นทักษะเฉพาะอาชีพเลขานุการหรือพนักงานพิมพ์ดีดเท่านั้น แต่ปัจจุบันได้กลายเป็นทักษะสำหรับทุกคนแล้ว เพราะการมีทักษะดังกล่าวติดตัวจะช่วยให้การใช้งานคอมพิวเตอร์สะดวกมากขึ้น สามารถป้อนข้อมูลข่าวสารได้มากและแม่นยำด้วยเวลาที่น้อยลง และที่สำคัญคือเป็นทักษะพื้นฐานที่ต้องการของนายจ้างเป็นอันดับต้นๆเลยทีเดียว"
-    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ"]
+      "topic":"ระบบปฏิบัติการ Microsoft Windows"
+    ,"body":"ในปัจจุบันระบบปฏิบัติการ Windows เป็นระบบปฏิบัติการที่นิยมใช้มากที่สุดในโลก ดังนั้นการรู้ความหมายและความสำคัญของระบบปฏิบัติการ ส่วนประกอบของวินโดวส์ ลักษณะพิเศษของวินโดวส์ และสามารถใช้งานวินโดวส์เบื้องต้นจึงเป็นสิ่งพื้นฐานสำคัญในการใช้คอมพิวเตอร์"
+    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ","พนักงานช่วยเหลือด้านคอมพิวเตอร์"]
     ,"course":[
-              {"name":"ฝึกพิมพ์ไทย-อังกฤษ","link":"http://www.thaiedu.net/typing/","pic":"imgcontent/"}
-              ,{"name":"ฝึกพิมพ์กับโยงใยไทยศึกษา","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":""}
-              ,{"name":"บทเรียนการพิมพ์สัมผัส","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":"imgcontent/"}
+              {"name":"บทเรียนระบบปฏิบัติการ Microsoft Windows ","link":"http://home.kku.ac.th/hslib/412141/412141_2548/c2windows.htm","pic":""}
               ]
     ,"center":[
-              {"name":"มูลนิธิส่งเสริมและพัฒนาคนพิการ รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","link":"http://www.tdf.or.th/14857695/มูลนิธิส่งเสริมและพัฒนาคนพิการ-รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","pic":"imgcontent/"}
-              ,{"name":"ฝึกทักษะคนพิการและบ้านวิถีชีวิตอิสระ","link":"http://snmrc57.snmrc.go.th/index.php?option=com_content&view=article&id=15&Itemid=16","pic":"imgcontent/"}
-              ]
+              {"name":"ศูนย์ฟื้นฟูอาชีพคนพิการพระประแดง ","link":"http://www.ppdrehabcenter.com/","pic":"imgcontent/center_1.png"}
+              ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการขอนแก่น  ","link":"http://www.rehab-khonkaen.dsdw.go.th/ ","pic":"imgcontent/center_2.png"}
+             ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการบ้านทองพูลเผ่าพนัส จังหวัดอุบลราชธานี  ","link":"http://archeep-center.blogspot.com/2014/11/blog-post.html#sthash.MoR9QJo8.dpuf ","pic":"imgcontent/center_3.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการหยาดฝน จังหวัดเชียงใหม่","link":"http://www.yardfon.go.th/ ","pic":"imgcontent/center_4.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดนครศรีธรรมราช ","link":"http://www.nakhonsivrc.go.th/ ","pic":"imgcontent/center_5.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดหนองคาย ","link":"http://www.rehab-nongkhai.dsdw.go.th/","pic":"imgcontent/center_6.png"}
+          ,{"name":"สถานฟื้นฟูสมรรถภาพคนพิการ ภาคตะวันออกเฉียงเหนือ - ศูนย์บริการสวัสดิการสังคมเฉลิมพระชนมพรรษา 5 รอบ สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ","link":"","pic":""}
+          ,{"name":"ศูนย์พัฒนาอาชีพคนพิการ ","link":"http://www.ddc.go.th/ ","pic":"imgcontent/center_8.png"}
+
+               ]
     }
 
     ,{
-      "topic":"สอนการพิมดีด"
-    ,"body":"ทักษะการพิมพ์ดีดซึ่งในอดีตเป็นทักษะเฉพาะอาชีพเลขานุการหรือพนักงานพิมพ์ดีดเท่านั้น แต่ปัจจุบันได้กลายเป็นทักษะสำหรับทุกคนแล้ว เพราะการมีทักษะดังกล่าวติดตัวจะช่วยให้การใช้งานคอมพิวเตอร์สะดวกมากขึ้น สามารถป้อนข้อมูลข่าวสารได้มากและแม่นยำด้วยเวลาที่น้อยลง และที่สำคัญคือเป็นทักษะพื้นฐานที่ต้องการของนายจ้างเป็นอันดับต้นๆเลยทีเดียว"
-    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ"]
+      "topic":"Microsoft Word"
+    ,"body":"โปรแกรม Microsoft Word เรียกได้ว่าเป็นโปรแกรมพื้นฐานอันดับแรกที่ทุกคนสามารถใช้งานได้ ดังนั้นยิ่งมรความสามารถในการใช้โปรแกรมนี้มากเท่าไร ยิ่งเป็นข้อได้เปรียบในการสมัครงานมากขึ้นเท่านั้น"
+    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ","พนักงานช่วยเหลือด้านคอมพิวเตอร์"]
     ,"course":[
-              {"name":"ฝึกพิมพ์ไทย-อังกฤษ","link":"http://www.thaiedu.net/typing/","pic":"imgcontent/"}
-              ,{"name":"ฝึกพิมพ์กับโยงใยไทยศึกษา","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":""}
-              ,{"name":"บทเรียนการพิมพ์สัมผัส","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":"imgcontent/"}
+              {"name":"Microsoft office Word 2003","link":"http://home.kku.ac.th/hslib/412141/412141_2548/c3word.htm","pic":""}
+              ,{"name":"การใช้โปรแกรมประมวลผลคำ","link":"http://www2.tsu.ac.th/cst/course/word2013/word2013.html ","pic":"imgcontent/it_low_4_online_2.png"}
               ]
     ,"center":[
-              {"name":"มูลนิธิส่งเสริมและพัฒนาคนพิการ รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","link":"http://www.tdf.or.th/14857695/มูลนิธิส่งเสริมและพัฒนาคนพิการ-รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","pic":"imgcontent/"}
-              ,{"name":"ฝึกทักษะคนพิการและบ้านวิถีชีวิตอิสระ","link":"http://snmrc57.snmrc.go.th/index.php?option=com_content&view=article&id=15&Itemid=16","pic":"imgcontent/"}
-              ]
+          {"name":"ศูนย์ฟื้นฟูอาชีพคนพิการพระประแดง ","link":"http://www.ppdrehabcenter.com/","pic":"imgcontent/center_1.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการขอนแก่น  ","link":"http://www.rehab-khonkaen.dsdw.go.th/ ","pic":"imgcontent/center_2.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการบ้านทองพูลเผ่าพนัส จังหวัดอุบลราชธานี  ","link":"http://archeep-center.blogspot.com/2014/11/blog-post.html#sthash.MoR9QJo8.dpuf ","pic":"imgcontent/center_3.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการหยาดฝน จังหวัดเชียงใหม่","link":"http://www.yardfon.go.th/ ","pic":"imgcontent/center_4.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดนครศรีธรรมราช ","link":"http://www.nakhonsivrc.go.th/ ","pic":"imgcontent/center_5.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดหนองคาย ","link":"http://www.rehab-nongkhai.dsdw.go.th/","pic":"imgcontent/center_6.png"}
+          ,{"name":"สถานฟื้นฟูสมรรถภาพคนพิการ ภาคตะวันออกเฉียงเหนือ - ศูนย์บริการสวัสดิการสังคมเฉลิมพระชนมพรรษา 5 รอบ สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ","link":"","pic":""}
+          ,{"name":"ศูนย์พัฒนาอาชีพคนพิการ ","link":"http://www.ddc.go.th/ ","pic":"imgcontent/center_8.png"}
+
+      ]
     }
 
     ,{
-      "topic":"สอนการพิมดีด"
-    ,"body":"ทักษะการพิมพ์ดีดซึ่งในอดีตเป็นทักษะเฉพาะอาชีพเลขานุการหรือพนักงานพิมพ์ดีดเท่านั้น แต่ปัจจุบันได้กลายเป็นทักษะสำหรับทุกคนแล้ว เพราะการมีทักษะดังกล่าวติดตัวจะช่วยให้การใช้งานคอมพิวเตอร์สะดวกมากขึ้น สามารถป้อนข้อมูลข่าวสารได้มากและแม่นยำด้วยเวลาที่น้อยลง และที่สำคัญคือเป็นทักษะพื้นฐานที่ต้องการของนายจ้างเป็นอันดับต้นๆเลยทีเดียว"
-    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ"]
+      "topic":"Microsoft PowerPoint"
+    ,"body":"โปรแกรม Microsoft PowerPoint 2007 เป็นโปรแกรมหนึ่งในตระกูล Microsoft Office เหมาะสำหรับการจัดสร้างงานนำเสนอข้อมูล (Presentation) สำหรับนำไปประยุกต์ใช้ในงานได้หลายประเภท เช่น การนำเสนอข้อมูลสินค้าและบริการ การจัดทำ Slide Show การออกแบบแผ่นพับ เป็นต้น"
+    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ","พนักงานช่วยเหลือด้านคอมพิวเตอร์"]
     ,"course":[
-              {"name":"ฝึกพิมพ์ไทย-อังกฤษ","link":"http://www.thaiedu.net/typing/","pic":"imgcontent/"}
-              ,{"name":"ฝึกพิมพ์กับโยงใยไทยศึกษา","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":""}
-              ,{"name":"บทเรียนการพิมพ์สัมผัส","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":"imgcontent/"}
+              {"name":"MS powerpoint2013","link":"http://www2.tsu.ac.th/cst/course/powerpoint2013/power_p.html ","pic":"imgcontent/it_low_5_online_1.png"}
+              ,{"name":"โปรแกรม Microsoft PowerPoint","link":"http://home.kku.ac.th/hslib/412141/412141_2548/c4ppt.htm ","pic":"imgcontent/it_low_5_online_2.png"}
               ]
     ,"center":[
-              {"name":"มูลนิธิส่งเสริมและพัฒนาคนพิการ รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","link":"http://www.tdf.or.th/14857695/มูลนิธิส่งเสริมและพัฒนาคนพิการ-รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","pic":"imgcontent/"}
-              ,{"name":"ฝึกทักษะคนพิการและบ้านวิถีชีวิตอิสระ","link":"http://snmrc57.snmrc.go.th/index.php?option=com_content&view=article&id=15&Itemid=16","pic":"imgcontent/"}
-              ]
+          {"name":"ศูนย์ฟื้นฟูอาชีพคนพิการพระประแดง ","link":"http://www.ppdrehabcenter.com/","pic":"imgcontent/center_1.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการขอนแก่น  ","link":"http://www.rehab-khonkaen.dsdw.go.th/ ","pic":"imgcontent/center_2.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการบ้านทองพูลเผ่าพนัส จังหวัดอุบลราชธานี  ","link":"http://archeep-center.blogspot.com/2014/11/blog-post.html#sthash.MoR9QJo8.dpuf ","pic":"imgcontent/center_3.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการหยาดฝน จังหวัดเชียงใหม่","link":"http://www.yardfon.go.th/ ","pic":"imgcontent/center_4.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดนครศรีธรรมราช ","link":"http://www.nakhonsivrc.go.th/ ","pic":"imgcontent/center_5.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดหนองคาย ","link":"http://www.rehab-nongkhai.dsdw.go.th/","pic":"imgcontent/center_6.png"}
+          ,{"name":"สถานฟื้นฟูสมรรถภาพคนพิการ ภาคตะวันออกเฉียงเหนือ - ศูนย์บริการสวัสดิการสังคมเฉลิมพระชนมพรรษา 5 รอบ สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ","link":"","pic":""}
+          ,{"name":"ศูนย์พัฒนาอาชีพคนพิการ ","link":"http://www.ddc.go.th/ ","pic":"imgcontent/center_8.png"}
+
+      ]
     }
 
     ,{
-      "topic":"สอนการพิมดีด"
-    ,"body":"ทักษะการพิมพ์ดีดซึ่งในอดีตเป็นทักษะเฉพาะอาชีพเลขานุการหรือพนักงานพิมพ์ดีดเท่านั้น แต่ปัจจุบันได้กลายเป็นทักษะสำหรับทุกคนแล้ว เพราะการมีทักษะดังกล่าวติดตัวจะช่วยให้การใช้งานคอมพิวเตอร์สะดวกมากขึ้น สามารถป้อนข้อมูลข่าวสารได้มากและแม่นยำด้วยเวลาที่น้อยลง และที่สำคัญคือเป็นทักษะพื้นฐานที่ต้องการของนายจ้างเป็นอันดับต้นๆเลยทีเดียว"
-    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ"]
+      "topic":"Microsoft Excel"
+    ,"body":"Microsoft Excel เป็นโปรแกรมประเภท สเปรดชีต (spreadsheet) หรือตารางคำนวณอิเล็กทรอนิกส์ที่ใช้เก็บบันทึกข้อมูลในลักษณะต่างๆ ซึ่งส่วนใหญ่มักเก็บข้อมูลประเภทการคำนวณ โดยจะเก็บข้อมูลลงในตารางสี่เหลี่ยมที่เรียกว่า เซล (Cell) ที่สามารถนำเอาเซลมาอ้างอิงใส่ในสูตร เพื่อให้โปรแกรมคำนวณหาผลลัพธ์จากข้อมูลที่บันทึกไว้ได้"
+    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ","พนักงานช่วยเหลือด้านคอมพิวเตอร์"]
     ,"course":[
-              {"name":"ฝึกพิมพ์ไทย-อังกฤษ","link":"http://www.thaiedu.net/typing/","pic":"imgcontent/"}
-              ,{"name":"ฝึกพิมพ์กับโยงใยไทยศึกษา","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":""}
-              ,{"name":"บทเรียนการพิมพ์สัมผัส","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":"imgcontent/"}
+              {"name":"การใช้โปรแกรม Microsoft Excel ","link":"http://home.kku.ac.th/hslib/412141/412141_2548/c5excel.htm","pic":""}
+              ,{"name":"การใช้ Microsoft Excel ","link":"http://www2.tsu.ac.th/cst/course/excel2013/index.html","pic":"it_low_6_online_2.png"}
               ]
     ,"center":[
-              {"name":"มูลนิธิส่งเสริมและพัฒนาคนพิการ รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","link":"http://www.tdf.or.th/14857695/มูลนิธิส่งเสริมและพัฒนาคนพิการ-รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","pic":"imgcontent/"}
-              ,{"name":"ฝึกทักษะคนพิการและบ้านวิถีชีวิตอิสระ","link":"http://snmrc57.snmrc.go.th/index.php?option=com_content&view=article&id=15&Itemid=16","pic":"imgcontent/"}
-              ]
+          {"name":"ศูนย์ฟื้นฟูอาชีพคนพิการพระประแดง ","link":"http://www.ppdrehabcenter.com/","pic":"imgcontent/center_1.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการขอนแก่น  ","link":"http://www.rehab-khonkaen.dsdw.go.th/ ","pic":"imgcontent/center_2.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการบ้านทองพูลเผ่าพนัส จังหวัดอุบลราชธานี  ","link":"http://archeep-center.blogspot.com/2014/11/blog-post.html#sthash.MoR9QJo8.dpuf ","pic":"imgcontent/center_3.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการหยาดฝน จังหวัดเชียงใหม่","link":"http://www.yardfon.go.th/ ","pic":"imgcontent/center_4.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดนครศรีธรรมราช ","link":"http://www.nakhonsivrc.go.th/ ","pic":"imgcontent/center_5.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดหนองคาย ","link":"http://www.rehab-nongkhai.dsdw.go.th/","pic":"imgcontent/center_6.png"}
+          ,{"name":"สถานฟื้นฟูสมรรถภาพคนพิการ ภาคตะวันออกเฉียงเหนือ - ศูนย์บริการสวัสดิการสังคมเฉลิมพระชนมพรรษา 5 รอบ สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ","link":"","pic":""}
+          ,{"name":"ศูนย์พัฒนาอาชีพคนพิการ ","link":"http://www.ddc.go.th/ ","pic":"imgcontent/center_8.png"}
+
+      ]
     }
 
     ,{
-      "topic":"สอนการพิมดีด"
-    ,"body":"ทักษะการพิมพ์ดีดซึ่งในอดีตเป็นทักษะเฉพาะอาชีพเลขานุการหรือพนักงานพิมพ์ดีดเท่านั้น แต่ปัจจุบันได้กลายเป็นทักษะสำหรับทุกคนแล้ว เพราะการมีทักษะดังกล่าวติดตัวจะช่วยให้การใช้งานคอมพิวเตอร์สะดวกมากขึ้น สามารถป้อนข้อมูลข่าวสารได้มากและแม่นยำด้วยเวลาที่น้อยลง และที่สำคัญคือเป็นทักษะพื้นฐานที่ต้องการของนายจ้างเป็นอันดับต้นๆเลยทีเดียว"
-    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ"]
+      "topic":"การใช้อินเทอร์เน็ตเบื้องต้น"
+    ,"body":"ความรู้พื้นฐานของการใช้งาน อินเตอร์เน็ต (Internet)นั้น ประกอบด้วยการใช้งานด้านการค้นหา ข้อมูลและสื่อสารข้อมูล ซึ่งผู้ที่สามารถใช้งานอินเทอร์เน็ตไดอย่างคล่องแคล่วเรียกได้ว่าเป็นผู้ที่มีข้อได้เปรียบมากทีเดียว"
+    ,"job":["เจ้าหน้าที่ธุรการ","พนักงานป้อนข้อมูล","พนักงานคอมพิวเตอร์","เลขานุการ","พนักงานช่วยเหลือด้านคอมพิวเตอร์","พนักงานค้นหาข้อมูล"]
     ,"course":[
-              {"name":"ฝึกพิมพ์ไทย-อังกฤษ","link":"http://www.thaiedu.net/typing/","pic":"imgcontent/"}
-              ,{"name":"ฝึกพิมพ์กับโยงใยไทยศึกษา","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":""}
-              ,{"name":"บทเรียนการพิมพ์สัมผัส","link":"http://www.thaiedu.net/typing/typing_thai.php?lesson=T0101","pic":"imgcontent/"}
+              {"name":"ความรู้เบื้องต้นเกี่ยวกับ Internet และวิธีการติดต่อเข้าสู่ ","link":"http://home.kku.ac.th/hslib/412141/412141_2548/c6Internet.htm","pic":""}
+              ,{"name":"การใช้งานคอมพิวเตอร์เบื้องต้นและการใช้งานอินเตอร์เน็ต","link":"http://www2.tsu.ac.th/cst/course/basic_computer/index.html ","pic":"imgcontent/it_low_7_online_2.png"}
               ]
     ,"center":[
-              {"name":"มูลนิธิส่งเสริมและพัฒนาคนพิการ รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","link":"http://www.tdf.or.th/14857695/มูลนิธิส่งเสริมและพัฒนาคนพิการ-รับสมัครคนพิการเข้าฝีกอบรมคอมพิวเตอร์","pic":"imgcontent/"}
-              ,{"name":"ฝึกทักษะคนพิการและบ้านวิถีชีวิตอิสระ","link":"http://snmrc57.snmrc.go.th/index.php?option=com_content&view=article&id=15&Itemid=16","pic":"imgcontent/"}
-              ]
+          {"name":"ศูนย์ฟื้นฟูอาชีพคนพิการพระประแดง ","link":"http://www.ppdrehabcenter.com/","pic":"imgcontent/center_1.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการขอนแก่น  ","link":"http://www.rehab-khonkaen.dsdw.go.th/ ","pic":"imgcontent/center_2.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการบ้านทองพูลเผ่าพนัส จังหวัดอุบลราชธานี  ","link":"http://archeep-center.blogspot.com/2014/11/blog-post.html#sthash.MoR9QJo8.dpuf ","pic":"imgcontent/center_3.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการหยาดฝน จังหวัดเชียงใหม่","link":"http://www.yardfon.go.th/ ","pic":"imgcontent/center_4.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดนครศรีธรรมราช ","link":"http://www.nakhonsivrc.go.th/ ","pic":"imgcontent/center_5.png"}
+          ,{"name":"ศูนย์ฟื้นฟูอาชีพคนพิการจังหวัดหนองคาย ","link":"http://www.rehab-nongkhai.dsdw.go.th/","pic":"imgcontent/center_6.png"}
+          ,{"name":"สถานฟื้นฟูสมรรถภาพคนพิการ ภาคตะวันออกเฉียงเหนือ - ศูนย์บริการสวัสดิการสังคมเฉลิมพระชนมพรรษา 5 รอบ สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ","link":"","pic":""}
+          ,{"name":"ศูนย์พัฒนาอาชีพคนพิการ ","link":"http://www.ddc.go.th/ ","pic":"imgcontent/center_8.png"}
+
+      ]
     }
   ];
 
